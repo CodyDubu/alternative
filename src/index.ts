84179026fe45c8f2bfb64a53c0f287f6fe39158c
chris <chrshawkes@yahoo.com>
2019-10-08 21:52:41 -0400

    adding parser

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 function parser(value) : Array<string> {
     let regex = new RegExp(/\{\{(.*?)}}/gi);
-    let test = value.match(regex);
-    return test;
+    let props = value.match(regex);
+    return props;
 }
 
-function buildElement(template : string, value: Array<string>) : void {
+function buildElement(template : string, values: Array<string>) : void {
     let variableMatches = parser(template);
-    document.body.innerHTML = template;
+    let props = parseProps(variableMatches, values);
+
+    // deliver parsed results
+    document.body.innerHTML = props.toString();
+}
+
+function parseProps(variables : Array<string>, values : Array<string>) : Array<string> {
+    let parsedArray : Array<string> = [];
+
+    for (let x = 0; x < variables.length; x++) {
+        let ele =  variables[x].substring(2, variables[x].length - 2);
+        ele = values[x];
+        parsedArray.push(ele);
+    }
+
+    return parsedArray;
 }
 
 buildElement(`<h1>Hello {{world}}</h1>`, ['test']);
